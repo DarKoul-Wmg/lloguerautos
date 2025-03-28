8c916a14271b29f16a739ca6d318001d3d75ff7b
DarKoul-Wmg <williammg0904@gmail.com>
2025-03-28 15:54:40 +0100

    fita 1 posada en martxa y fita 2 activar model a admin panel

--- lloguer/admin.py
+++ lloguer/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from lloguer.models import *
 
 # Register your models here.
+
+from django.contrib import admin
+from lloguer.models import Automobil
+
+class AutomobilAdmin(admin.ModelAdmin):
+    list_display = ('matricula', 'marca', 'model')  
+    search_fields = ('matricula', 'marca', 'model')
+
+admin.site.register(Automobil, AutomobilAdmin)
